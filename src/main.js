e63603adc9dfb1dc51e2cc3860ee7a4675de7d83
zsoit <wsparcie.exe@gmail.com>
2021-01-03 00:01:58 +0100

    Update All

--- src/main.js
+++ src/main.js
@@ -1,55 +1,166 @@
-const userList = document.querySelector('.terminal');
+const lastP = document.querySelector('.terminal');
 const listInput = document.querySelector('input');
 const addListBtn = document.querySelector('button');
 
-let input = document.getElementById("myInput");
-input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key === "Enter" || evt.key === "Enter");
+    } else {
+        isEscape = (evt.keyCode === 27);
+    }
+    if (isEscape) {
         showLine();
     }
-});
-
+};
 
 function showLine() {
 
     let command = document.querySelector('input').value;
 
-    let commandResult;
-
-    if (command == "ls") {
-        commandResult = 'Dokumenty  Muzyka readme.txt';
-    } else if (command == "clear" || command == "clean") {
-        window.location.reload(true);
-        commandResult = ' ';
-    } else if (command == "neofetch") {
-        commandResult = `OS: Swiat Linuxa  
-            Host: YouTube 
-            Kernel: 4,42tys. sub 
-            Packages: 112(filmy) 
-            Shell: bash 5.0.3 
-            CPU: Ryzen 7 
-            GPU: RADEON
-            Memory: 135Mib/ 3029Mib`;
-    } else if (command == "free -h") {
-        commandResult = 'WOLNE 200GB';
-
-
-    } else {
-        commandResult = command + ' : bash not found command';
+    function addElementHTML(html) {
+        var element = document.createElement('span');
+        element.innerHTML = 'swiat@lnuxa:~$ ' + command + '<br>' + html + '<br><br>';
+        document.querySelector('.terminalOut').appendChild(element);
     }
 
-    const newLI = document.createElement("P");
-    const liContent = document.createTextNode('swiat@lnuxa:~$' + ' ' + command + ' ');
-    newLI.appendChild(liContent);
-    userList.appendChild(newLI);
-    const newi = document.createElement("p");
-    const iconent = document.createTextNode(commandResult);
-    newi.appendChild(iconent);
-    userList.appendChild(newi);
+    //COMAND
+    if (command == "ls") addElementHTML(`
+    readme.txt
+    Siema.txt
+    `);
+    else if (command == "ls -l") {
+        addElementHTML(`
+    <table>
+    <tr>
+        <td>-rwxrwxrwx</td>
+        <td>1</td>
+        <td>root</td>
+        <td>root</td>
+        <td width="50px"></td>
+        <td>124</td>
 
-    // document.querySelector('.terminal').innerHTML = commandResult;
+    </tr>
+    <tr>
+        <td>-rwxrwxrwx</td>
+        <td>1</td>
+        <td>root</td>
+        <td>root</td>
+        <td width="50px"></td>
+        <td>124</td>
+
+   
+    </tr>
+    <tr>
+        <td>-rwxrwxrwx</td>
+        <td>1</td>
+        <td>root</td>
+        <td>root</td>
+        <td width="50px"></td>
+        <td> 25</td>
+ 
+    </tr>
+    <tr>
+        <td>-rwxrwxrwx</td>
+        <td>1</td>
+        <td>root</td>
+        <td>root</td>
+        <td width="50px"></td>
+        <td> 65</td>
+   
+    </tr>
+    <tr>
+        <td>-rwxrwxrwx</td>
+        <td>1</td>
+        <td>root</td>
+        <td>root</td>
+        <td width="50px"></td>
+  
+    </tr>
+    <tr>
+        <td>-rwxrwxrwx</td>
+        <td>1</td>
+        <td>root</td>
+        <td>root</td>
+        <td width="50px"></td>
+   
+    </tr>
+    <tr>
+        <td>-rwxrwxrwx</td>
+        <td>1</td>
+        <td>root</td>
+        <td>root</td>
+        <td width="50px"></td>
+
+    </tr>
+</table>
+
+<table>
+<tr>
+    <td>
+</tr>
+</table>
+    `);
+    } else if (command == "clean") window.location.reload(true);
+
+    else if (command == "neofetch") addElementHTML(`
+    <img src="neo.png" width="150px" style="float: left; padding:20px;">
+    OS: Swiat Linuxa <br>
+    Host: YouTube <br>
+    Kernel: 4,42tys. sub
+    <br> Packages: 112(filmy)
+    <br> Shell: bash 5.0.3
+    <br> CPU: Ryzen 7
+    <br> GPU: RADEON
+    <br> Memory: 135Mib/ 3029Mib <br>`);
+    else if (command == '') addElementHTML(' ');
+
+
+    else addElementHTML("' " + command + " ' : bash command not found");
 
     document.querySelector('input').value = "";
 
 
 }
+
+function exit() {
+    document.querySelector('#console').style.display = "none";
+    document.querySelector('.icon').style.display = "block";
+    document.querySelector('#app').innerHTML = 'Pulpit';
+    document.querySelector('title').innerHTML = 'Pulpit';
+
+
+
+}
+
+
+function show() {
+    document.querySelector('#console').style.display = "block";
+    document.querySelector('.icon').style.display = "none";
+    document.querySelector('#app').innerHTML = 'Terminal';
+    document.querySelector('title').innerHTML = 'Terminal';
+
+
+
+}
+
+function firefox() {
+    document.querySelector('#app').innerHTML = 'Firefox';
+    document.querySelector('#console').style.display = "none";
+    document.querySelector('.icon').style.display = "none";
+    document.querySelector('.firefox').style.display = "block";
+    document.querySelector(frame).style.display = "block";
+
+}
+let hour = new Date().getHours();
+let minute = new Date().getMinutes();
+let day = new Date().getDay();
+
+if (minute <= 9) {
+    minute = '0' + minute;
+}
+
+
+document.querySelector('#time').innerHTML = hour + ":" + minute;
